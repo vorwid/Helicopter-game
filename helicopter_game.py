048975b497dfbc00f0b6b4ca1ffa293b7ad8999f
vorwid <vorwid@gmail.com>
2020-02-10 19:51:26 +0100

    Update helicopter_game.py

--- helicopter_game.py
+++ helicopter_game.py
@@ -6,3 +6,10 @@ szer = 600
 wys = 600
 
 screen = pygame.display.set_mode((szer, wys))
+
+def napisz(tekst, x, y, rozmiar):
+    czcionka = pygame.font.SysFont("Arial", rozmiar)
+    rend = czcionka.render(tekst, 1, (255,100,100))
+    screen.blit(rend, (x,y))  # this line is use to put render text in graphic window
+    
+napisz("Nacisnij spacje zeby zaczac", 80, 150, 20)
