16314be0271ece165ca6466f2c89972470a9ccb4
vorwid <vorwid@gmail.com>
2020-02-11 21:55:55 +0100

    Update helicopter_game.py

--- helicopter_game.py
+++ helicopter_game.py
@@ -11,9 +11,14 @@ def napisz(tekst, x, y, rozmiar):
     czcionka = pygame.font.SysFont("Arial", rozmiar)
     rend = czcionka.render(tekst, 1, (255,100,100))
     screen.blit(rend, (x,y))  # this line is use to put render text in graphic window
-    
-napisz("Nacisnij spacje zeby zaczac", 80, 150, 20)
 
-pygame.display.update()
+
+while True:                                     # This loop close game window
+    for event in pygame.event.get():
+        pygame.quit()
+        quit()
+
+    napisz("Nacisnij spacje zeby zaczac", 80, 150, 20)
+    pygame.display.update()
 
 
