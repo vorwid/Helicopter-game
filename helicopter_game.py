0fa6a96f1b568cf5703a1cb6966dd966cdf67b3b
vorwid <vorwid@gmail.com>
2020-02-12 18:28:26 +0100

    Update helicopter_game.py

--- helicopter_game.py
+++ helicopter_game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -12,13 +13,27 @@ def napisz(tekst, x, y, rozmiar):
     rend = czcionka.render(tekst, 1, (255,100,100))
     screen.blit(rend, (x,y))  # this line is use to put render text in graphic window
 
+copokazuje = "menu"
+
+class Przeszkoda():
+    def __init__(self, x, szerokosc):
+        self.x=x
+        self.szerokosc = szerokosc
+        self.y_gora = y
+        self.wys_gora = random.randint(150, 250)
+        self.odstep = 200
+        self.y_dol = self.wys_gora + self.odstep
 
 while True:                                     # This loop close game window
     for event in pygame.event.get():
-        pygame.quit()
-        quit()
-
-    napisz("Nacisnij spacje zeby zaczac", 80, 150, 20)
+        if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+    if copokazuje == "menu":
+            napisz("Nacisnij spacje zeby zaczac", 80, 150, 20)
+            grafika = pygame.image.load(os.path.join("helicoper.png"))
+            screen.blit(grafika, (80, 30))
+    
     pygame.display.update()
 
 
