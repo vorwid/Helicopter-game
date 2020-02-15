a699565843bc27ea2ed423fa89f98be42eea9bf1
vorwid <vorwid@gmail.com>
2020-02-15 09:47:35 +0100

    Update helicopter_game.py

--- helicopter_game.py
+++ helicopter_game.py
@@ -16,7 +16,7 @@ def napisz(tekst, x, y, rozmiar):
 
 copokazuje = "rozgrywka"
 
-class Pszeszkoda():
+class Przeszkoda():
     def __init__(self, x, szerokosc):
         self.x = x
         self.szerokosc = szerokosc
@@ -31,23 +31,34 @@ class Pszeszkoda():
     def rysuj(self):
         pygame.draw.rect(screen, self.kolor, self.ksztalt_gora, 0)
         pygame.draw.rect(screen, self.kolor, self.ksztalt_dol, 0)
+    def ruch(self, v):
+        self.x = self.x - v
+        self.ksztalt_gora = pygame.Rect(self.x, self.y_gora, self.szerokosc, self.wys_gora)
+        self.ksztalt_dol = pygame.Rect(self.x, self.y_dol, self.szerokosc, self.wys_dol)
+                                
 
-pszeszkody = []
+przeszkody = []
 for i in range(21):
-    pszeszkody.append(Pszeszkoda(i*szer/20, szer/20))
+    przeszkody.append(Przeszkoda(i*szer/20, szer/20))
 
 while True:                                     # This loop close game window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+    screen.fill((0, 0, 0))
     if copokazuje == "menu":
             napisz("Nacisnij spacje zeby zaczac", 80, 150, 20)
             grafika = pygame.image.load(os.path.join("helicoper.png"))
             screen.blit(grafika, (80, 30))
     elif copokazuje == "rozgrywka":
-        for p in pszeszkody:
+        for p in przeszkody:
+            p.ruch(1)
             p.rysuj()
+        for p in przeszkody:
+            if p.x <= -p.szerokosc:
+                przeszkody.remove(p)
+                przeszkody.append((Przeszkoda(szer, szer/20)))
     
     pygame.display.update()
 
